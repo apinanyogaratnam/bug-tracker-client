cef5155af52e362edfe37ef69de86563327efb9b
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 13:50:14 -0400

    refactor: using dialog component

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
 import useProjects from '../../components/hooks/useProjects';
 import { useState } from 'react';
+import Dialog from '../../components/Dialog';
 
 export default function Projects() {
     const { projects, loading } = useProjects(1);
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const closeDialog = () => {
+        setIsDialogOpen(false);
+    }
+
     return (
         <div>
             <h1>Projects</h1>
@@ -12,49 +18,22 @@ export default function Projects() {
                 <p>Loading...</p>
             ) : (
                 <div>
-                    { isDialogOpen && (
-                            <div style={{
-                                position: 'absolute',
-                                top: '0',
-                                left: '0',
-                                width: '100%',
-                                height: '100%',
-                                background: 'rgba(0,0,0,0.5)',
-                            }}>
-                                <dialog
-                                    style={{
-                                        zIndex: '1',
-                                    }} open={isDialogOpen}>
-                                    <form>
-                                        <label>
-                                            Name:
-                                            <input type="text" />
-                                        </label>
-                                        <label>
-                                            Description:
-                                            <input type="text" />
-                                        </label>
-                                        <button>Submit</button>
-                                    </form>
-                                </dialog>
-                            </div>
-                        )
-                    }
+                    <Dialog isOpened={isDialogOpen} closeDialog={closeDialog} />
+                    <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
+                        New Project
+                    </button>
                     {projects.map((project, index) => (
-                                <div key={index}>
-                                    <Link href={`/projects/${project.project_id}`}>
-                                        <button>
-                                            <div>
-                                                <h2>{project.name}</h2>
-                                                <p>{project.description}</p>
-                                            </div>
-                                        </button>
-                                    </Link>
-                                </div>
-                            ))}
-                            <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
-                                New Project
-                            </button>
+                        <div key={index}>
+                            <Link href={`/projects/${project.project_id}`}>
+                                <button>
+                                    <div>
+                                        <h2>{project.name}</h2>
+                                        <p>{project.description}</p>
+                                    </div>
+                                </button>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
