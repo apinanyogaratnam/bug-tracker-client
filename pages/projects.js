67dae1a26b0fb1256425fc2836c349776695ccb2
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 12:04:46 -0400

    refactor: displaying user's projects to projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+import useProjects from '../components/hooks/useProjects';
+
 export default function Projects() {
+    const { projects, loading } = useProjects(1);
     return (
         <div>
             <h1>Projects</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {projects.map((project, index) => (
+                        <div key={index}>
+                            <li>
+                                <Link href="/projects/[project_id]" as={`/projects/${project.id}`}>
+                                    <a>{project.name}</a>
+                                </Link>
+                            </li>
+                        </div>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
