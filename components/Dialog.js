c16026d5d28ea8b0a474564919d3c3d85fd49018
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 13:45:11 -0400

    feat: created dialog component

---
+++ components/Dialog.js
@@ -0,0 +1,33 @@
+export default function Dialog({isOpened = false}) {
+    return (
+        <div>
+            { isOpened && (
+                <div style={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    width: '100%',
+                    height: '100%',
+                    background: 'rgba(0,0,0,0.5)',
+                }}>
+                    <dialog
+                        style={{
+                            zIndex: '1',
+                        }} open={isOpened}>
+                        <form>
+                            <label>
+                                Name:
+                                <input type="text" />
+                            </label>
+                            <label>
+                                Description:
+                                <input type="text" />
+                            </label>
+                            <button>Submit</button>
+                        </form>
+                    </dialog>
+                </div>
+            )}
+        </div>
+    );
+}
