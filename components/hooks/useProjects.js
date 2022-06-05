d892af68421b262df39ecddeb311ef4b64faff4c
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 12:05:13 -0400

    fix: loading correct api url and migrated from fetch to axios

--- components/hooks/useProjects.js
+++ components/hooks/useProjects.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function useProjects(user_id) {
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const fetchData = async (user_id) => {
-        const response = await fetch(`${process.env.API_URL}/projects?user_id=${user_id}`);
-        const data = await response.json();
+        const API_URL = process.env.NEXT_PUBLIC_API_URL;
+        const response = await axios.get(
+            `${API_URL}/projects?user_id=${user_id}`
+        )
+        const data = await response.data;
         setProjects(data);
         setLoading(false);
     }
