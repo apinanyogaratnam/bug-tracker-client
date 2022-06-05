0c314df10a3d4e627c1926ede5fd36af6c0b0254
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 15:14:48 -0400

    style: styling projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import useProjects from '../../components/hooks/useProjects';
 import { useState } from 'react';
 import Dialog from '../../components/Dialog';
+import styles from '../../styles/Projects.module.css';
 
 export default function Projects() {
     const { projects, loading } = useProjects(1);
@@ -12,28 +13,30 @@ export default function Projects() {
     }
 
     return (
-        <div>
+        <div className={styles['main-container']}>
             <h1>Projects</h1>
             {loading ? (
                 <p>Loading...</p>
             ) : (
                 <div>
                     <Dialog isOpened={isDialogOpen} closeDialog={closeDialog} />
-                    <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
-                        New Project
-                    </button>
-                    {projects.map((project, index) => (
-                        <div key={index}>
-                            <Link href={`/projects/${project.project_id}`}>
-                                <button>
-                                    <div>
-                                        <h2>{project.name}</h2>
-                                        <p>{project.description}</p>
-                                    </div>
-                                </button>
-                            </Link>
-                        </div>
-                    ))}
+                    <div className={styles['projects-container']}>
+                        {projects.map((project, index) => (
+                            <div key={index} className={styles['project-container']}>
+                                <Link href={`/projects/${project.project_id}`}>
+                                    <button>
+                                        <div>
+                                            <h2>{project.name}</h2>
+                                            <p>{project.description}</p>
+                                        </div>
+                                    </button>
+                                </Link>
+                            </div>
+                        ))}
+                        <button className={styles['project-container']} onClick={() => setIsDialogOpen(!isDialogOpen)}>
+                            New Project
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
