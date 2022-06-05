c43749c123594b3264e62b71e3125aa7fdb6785a
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 11:05:15 -0400

    feat: created use projects hook

---
+++ components/hooks/useProjects.js
@@ -0,0 +1,20 @@
+export default function useProjects(user_id) {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async (user_id) => {
+        const response = await fetch(`${process.env.API_URL}/projects?user_id=${user_id}`);
+        const data = await response.json();
+        setProjects(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData(user_id);
+    }, [user_id]);
+
+    return {
+        projects,
+        loading
+    };
+}
