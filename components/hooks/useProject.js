d15e847ef07a36d2c6c9e04418a4cd773c01cd2d
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 17:32:52 -0400

    fix: changed function name from Projects to Project

--- components/hooks/useProject.js
+++ components/hooks/useProject.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function useProjects(project_id) {
+export default function useProject(project_id) {
     const [project, setProject] = useState([]);
     const [loading, setLoading] = useState(true);
 
