38898857d1008ccae7e768353340770ac15635af
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 12:55:00 -0400

    feat: created dialog for creating a new project

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import useProjects from '../../components/hooks/useProjects';
+import { useState } from 'react';
 
 export default function Projects() {
     const { projects, loading } = useProjects(1);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
     return (
         <div>
             <h1>Projects</h1>
@@ -10,6 +12,22 @@ export default function Projects() {
                 <p>Loading...</p>
             ) : (
                 <div>
+                    <dialog open={isDialogOpen}
+                        style={{
+                            zIndex: '1',
+                        }}>
+                        <form>
+                            <label>
+                                Name:
+                                <input type="text" />
+                            </label>
+                            <label>
+                                Description:
+                                <input type="text" />
+                            </label>
+                            <button>Submit</button>
+                        </form>
+                    </dialog>
                     {projects.map((project, index) => (
                         <div key={index}>
                             <Link href={`/projects/${project.project_id}`}>
@@ -22,7 +40,7 @@ export default function Projects() {
                             </Link>
                         </div>
                     ))}
-                    <button>
+                    <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
                         New Project
                     </button>
                 </div>
