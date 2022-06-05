b0f09e24b488ce20d0ac84ddd02e1b7c1cb7803b
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-05 13:25:44 -0400

    feat: greying background on dialog opened

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -12,37 +12,49 @@ export default function Projects() {
                 <p>Loading...</p>
             ) : (
                 <div>
-                    <dialog open={isDialogOpen}
-                        style={{
-                            zIndex: '1',
-                        }}>
-                        <form>
-                            <label>
-                                Name:
-                                <input type="text" />
-                            </label>
-                            <label>
-                                Description:
-                                <input type="text" />
-                            </label>
-                            <button>Submit</button>
-                        </form>
-                    </dialog>
+                    { isDialogOpen && (
+                            <div style={{
+                                position: 'absolute',
+                                top: '0',
+                                left: '0',
+                                width: '100%',
+                                height: '100%',
+                                background: 'rgba(0,0,0,0.5)',
+                            }}>
+                                <dialog
+                                    style={{
+                                        zIndex: '1',
+                                    }} open={isDialogOpen}>
+                                    <form>
+                                        <label>
+                                            Name:
+                                            <input type="text" />
+                                        </label>
+                                        <label>
+                                            Description:
+                                            <input type="text" />
+                                        </label>
+                                        <button>Submit</button>
+                                    </form>
+                                </dialog>
+                            </div>
+                        )
+                    }
                     {projects.map((project, index) => (
-                        <div key={index}>
-                            <Link href={`/projects/${project.project_id}`}>
-                                <button>
-                                    <div>
-                                        <h2>{project.name}</h2>
-                                        <p>{project.description}</p>
-                                    </div>
-                                </button>
-                            </Link>
-                        </div>
-                    ))}
-                    <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
-                        New Project
-                    </button>
+                                <div key={index}>
+                                    <Link href={`/projects/${project.project_id}`}>
+                                        <button>
+                                            <div>
+                                                <h2>{project.name}</h2>
+                                                <p>{project.description}</p>
+                                            </div>
+                                        </button>
+                                    </Link>
+                                </div>
+                            ))}
+                            <button onClick={() => setIsDialogOpen(!isDialogOpen)}>
+                                New Project
+                            </button>
                 </div>
             )}
         </div>
