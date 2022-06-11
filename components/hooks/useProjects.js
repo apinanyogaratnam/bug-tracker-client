9749974d687ef045311ff9505acdcc7f1504a336
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-11 19:51:36 -0400

    refactor: error handling server errors

--- components/hooks/useProjects.js
+++ components/hooks/useProjects.js
@@ -8,12 +8,24 @@ export default function useProjects(user_id) {
     useEffect(() => {
         const fetchData = async () => {
             const API_URL = process.env.NEXT_PUBLIC_API_URL;
-            const response = await axios.get(
-                `${API_URL}/projects?user_id=${user_id}`
-            )
-            const data = await response.data;
-            setProjects(data);
-            setLoading(false);
+            let response;
+            try {
+                const response = await axios.get(
+                    `${API_URL}/projects?user_id=${user_id}`
+                )
+                const data = await response.data;
+                setProjects(data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                if (error.response.status !== 0) {
+                    setProjects([]);
+                    setLoading(false);
+                    return;
+                }
+                setProjects(null);
+                setLoading(false);
+            }
         }
         fetchData();
     }, [projects, user_id]);
@@ -21,6 +33,5 @@ export default function useProjects(user_id) {
     return {
         projects,
         loading,
-        setProjects,
     };
 }
