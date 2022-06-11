8e1da6eec8951a6e57e565930ec0c1a3826968dc
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-11 19:05:55 -0400

    refactor: testing redux state management

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const user = useSelector(state => state.user);
+  console.log('user', user);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Bug Tracker</h1>
