c4dc75188ca0cd4e0c402efe269acabedda9698e
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 14:33:29 -0400

    refactor: wrapped main application component with user provider from auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <UserProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </UserProvider>
   );
 }
 
