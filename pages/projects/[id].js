179d75c73d9b07fffdc927877e8af2fce88950a0
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 12:25:56 -0400

    refactor: added placeholder visuals

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -108,6 +108,7 @@ export default function Project() {
                                                                         </Draggable>
                                                                     )
                                                                 })}
+                                                                {provided.placeholder}
                                                             </div>
                                                         )}
                                                     </Droppable>
