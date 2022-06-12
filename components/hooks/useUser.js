f92b1a7c986890cc14c978f258ee51832203ccf2
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 15:38:47 -0400

    feat: created useUser hook

---
+++ components/hooks/useUser.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+export default function useUser(user) {
+    const [fetchedUser, setFetchedUser] = useState({});
+    const [fetchedLoading, setFetchedLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async (user) => {
+            if (!user || error) return { user, loading };
+            const email = user.email;
+            const API_URL = process.env.NEXT_PUBLIC_API_URL;
+            try {
+                const { data } = await axios.get(`${API_URL}/user/${email}`);
+                setFetchedUser(data);
+                setFetchedLoading(false);
+            } catch (error) {
+                console.log(error);
+                if (error.response.status != 0) {
+                    setFetchedUser({});
+                    setFetchedLoading(false);
+                    return;
+                }
+                setFetchedUser(null);
+                setLoading(false);
+            }
+        }
+        fetchData(user);
+    }, [user]);
+
+    return {
+        fetchedUser,
+        fetchedLoading,
+    };
+}
