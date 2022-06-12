ea443e5a66f81af9c84f23a88af14dff38b4d8ff
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 16:09:23 -0400

    fix: client side rendering router

--- pages/index.js
+++ pages/index.js
@@ -10,35 +10,29 @@ import { useUser as useAuth0User } from '@auth0/nextjs-auth0';
 export default function Home() {
   const router = useRouter();
   const { user: auth0User, error } = useAuth0User();
-  console.log('auth0 user', auth0User);
   const { fetchedUser: user, fetchedLoading: loading } = useUser(auth0User);
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
-
-  if (user) {
-    // email = user.email;
-    // name = user.name;
-    // picture = user.picture;
-    // sub = user.sub;
-    console.log('fetched user', user);
-    setUserId(user.user_id);
-    router.push('/projects');
-  }
 
   const handleAuthentication = () => {
     router.push('/api/auth/login');
-  }
+  };
+
+  const handleRouteToProjects = () => {
+    if (error) {
+      return <p>Error: {error.message}</p>;
+    }
+
+    if (user && user != {}) {
+      console.log('fetched user', user);
+      setUserId(user.user_id);
+      router.push('/projects');
+    }
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Bug Tracker</h1>
-      <button onClick={handleAuthentication}>Login or Signup</button>
+      { !auth0User && <button onClick={handleAuthentication}>Login or Signup</button> }
+      { auth0User && <button onClick={handleRouteToProjects}>Go to Projects</button>}
     </div>
   )
 }
