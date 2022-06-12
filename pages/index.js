43c38877e948f6be0e933b2ab7652e2828d11865
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-12 15:39:00 -0400

    refactor: using useUser hook in main page

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useSelector } from 'react-redux'
 import styles from '../styles/Home.module.css'
-import { useUser as useAuth0User } from '@auth0/nextjs-auth0';
 import { useRouter } from 'next/router';
+import useUser from '../components/hooks/useUser';
+import { setUserId } from '../redux/slices/userSlice';
+import { useUser as useAuth0User } from '@auth0/nextjs-auth0';
 
 export default function Home() {
   const router = useRouter();
-  const { user, error, loading } = useAuth0User();
-  let email, name, picture, sub;
+  const { user: auth0User, error } = useAuth0User();
+  console.log('auth0 user', auth0User);
+  const { fetchedUser: user, fetchedLoading: loading } = useUser(auth0User);
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
 
   if (user) {
-    email = user.email;
-    name = user.name;
-    picture = user.picture;
-    sub = user.sub;
+    // email = user.email;
+    // name = user.name;
+    // picture = user.picture;
+    // sub = user.sub;
+    console.log('fetched user', user);
+    setUserId(user.user_id);
     router.push('/projects');
   }
 
