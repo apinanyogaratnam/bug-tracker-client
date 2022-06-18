3eb8034b655d101d7582102d0f7050c14ce7ac72
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 13:22:05 -0400

    refactor: alerting browser if failed to create task

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -77,6 +77,7 @@ export default function Project() {
             // TODO: update state
         } catch (error) {
             console.log(error);
+            alert('Error creating task');
         }
     }
 
