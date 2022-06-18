d0b07ec779f3d0ffb6494f49e419200d2932855c
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 11:46:15 -0400

    feat: created project slice

---
+++ redux/slices/projectSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    project_id: null,
+    name: '',
+    columns: {},
+};
+
+export const projectSlice = createSlice({
+    name: 'project',
+    initialState,
+    reducers: {
+        setProjects: (state, action) => {
+            state = action.payload;
+        },
+        setColumns: (state, action) => {
+            state.columns = action.payload;
+        },
+    },
+});
+
+export const {
+    setProject,
+} = projectSlice.actions;
+
+export default projectSlice.reducer;
