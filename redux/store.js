4c9a5f16a18e9f0b74159a47e4b1b7353bbbc465
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-18 11:51:22 -0400

    fix: added project to store redux state

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice';
 import projectsSlice from './slices/projectsSlice';
+import projectSlice from './slices/projectSlice';
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
         projects: projectsSlice,
+        project: projectSlice,
     },
 });
